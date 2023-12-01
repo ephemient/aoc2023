7066ec5728ea2b8c8ebf5bbd56b32f9826164d01
Daniel Lin <ephemient@gmail.com>
2023-12-01 06:36:53 -0500

    Add benchmarks

---
+++ hs/bench/Main.hs
@@ -0,0 +1,23 @@
+module Main (main) where
+
+import Criterion.Main (bench, bgroup, defaultMain, env, nf)
+import Data.Foldable (find)
+import Data.Maybe (fromMaybe)
+import Data.Text (Text)
+import qualified Data.Text.IO as TIO (readFile)
+import qualified Day1 (part1, part2)
+import System.Environment (lookupEnv)
+import System.FilePath (combine)
+
+getDayInput :: Int -> IO Text
+getDayInput i = do
+    dataDir <- fromMaybe "." . find (not . null) <$> lookupEnv "AOC2023_DATADIR"
+    TIO.readFile . combine dataDir $ "day" ++ show i ++ ".txt"
+
+main :: IO ()
+main = defaultMain
+  [ env (getDayInput 1) $ \input -> bgroup "Day 1"
+      [ bench "part 1" $ nf Day1.part1 input
+      , bench "part 2" $ nf Day1.part2 input
+      ]
+  ]
