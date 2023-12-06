887d51e91a1113e5dd255b65613b32cb5a224a65
Daniel Lin <ephemient@gmail.com>
2023-12-06 00:42:00 -0500

    auto-commit

--- kt/aoc2023-lib/src/commonMain/kotlin/com/github/ephemient/aoc2023/Day6.kt
+++ kt/aoc2023-lib/src/commonMain/kotlin/com/github/ephemient/aoc2023/Day6.kt
@@ -6,16 +6,16 @@ import kotlin.math.sqrt
 
 class Day6(private val input: String) {
     private val races = input.lines().let { (line1, line2) ->
-        NUMBER.findAll(line1).map { it.value.toInt() } zip
-            NUMBER.findAll(line2).map { it.value.toInt() }
+        NUMBER.findAll(line1).map { it.value.toLong() } zip
+            NUMBER.findAll(line2).map { it.value.toLong() }
     }
 
-    fun part1(): Int = races.fold(1) { acc, (time, distance) ->
+    fun part1(): Long = races.fold(1) { acc, (time, distance) ->
         acc * winCount(time, distance)
     }
 
-    fun part2(): Int {
-        val (time, distance) = NUMBER.findAll(input.replace(" ", "")).map { it.value.toInt() }.toList()
+    fun part2(): Long {
+        val (time, distance) = NUMBER.findAll(input.replace(" ", "")).map { it.value.toLong() }.toList()
         println(time)
         println(distance)
         return winCount(time, distance)
@@ -24,14 +24,14 @@ class Day6(private val input: String) {
     companion object {
         private val NUMBER = """\d+""".toRegex()
 
-        private fun winCount(time: Int, distance: Int): Int {
+        private fun winCount(time: Long, distance: Long): Long {
             // x * (time - x) > distance
             // x^2 - time*x < -distance
             // x^2 - time*x + (time/2)^2 < (time/2)^2 - distance
             // abs(x - time/2) < sqrt((time/2)^2 - distance)
             val b = time / 2.0
             val d = sqrt(b * b - distance)
-            return (ceil(b - d) - floor(b + d) + 1).toInt()
+            return (ceil(b - d) - floor(b + d) + 1).toLong()
         }
     }
 }
