290ae9021b07822ac19a31fd7b272ea2dcbc9acc
Daniel Lin <ephemient@gmail.com>
2023-12-06 00:27:14 -0500

    auto-commit

--- kt/aoc2023-lib/src/commonMain/kotlin/com/github/ephemient/aoc2023/Day6.kt
+++ kt/aoc2023-lib/src/commonMain/kotlin/com/github/ephemient/aoc2023/Day6.kt
@@ -1,10 +1,5 @@
 package com.github.ephemient.aoc2023
 
-import kotlin.math.ceil
-import kotlin.math.floor
-import kotlin.math.roundToInt
-import kotlin.math.sqrt
-
 class Day6(private val input: String) {
     private val races = input.lines().let { (line1, line2) ->
         NUMBER.findAll(line1).map { it.value.toInt() } zip
@@ -26,15 +21,28 @@ class Day6(private val input: String) {
         private val NUMBER = """\d+""".toRegex()
 
         private fun winCount(time: Int, distance: Int): Int {
-            // x * (time - x) > distance
-            // x^2 - time*x + time^2/4 < time^2/4 - distance
-            // (x - time/2)^2 < sqrt(time^2/4 - distance)
-            // x < time/2 + sqrt(time^2/4 - distance)
-            val lo = time / 2.0 - sqrt(time * time / 4.0 - distance)
-            val hi = time / 2.0 + sqrt(time * time / 4.0 - distance)
+            val lo = run {
+                var lo = 0
+                var hi = time / 2 + 1
+                while (lo < hi) {
+                    val x = lo + (hi - lo) / 2
+                    val y = x * (time - x)
+                    if (y <= distance) lo = x + 1 else hi = x
+                }
+                hi
+            }
+            val hi = run {
+                var lo = time / 2
+                var hi = time + 1
+                while (lo < hi) {
+                    val x = lo + (hi - lo) / 2
+                    val y = x * (time - x)
+                    if (y <= distance) hi = x + 1 else lo = x
+                }
+                lo
+            }
             println("$time/$distance $lo..$hi")
-            check(0 <= lo && lo <= hi && hi <= time)
-            return (floor(Double.fromBits(hi.toRawBits() - 1)) - ceil(Double.fromBits(lo.toRawBits() + 1)) + 1).roundToInt()
+            return hi - lo + 1
         }
     }
 }
