fb497127d136af4cba697a558fb375b9d3de6039
Daniel Lin <ephemient@gmail.com>
2023-12-06 00:09:32 -0500

    auto-commit

--- kt/aoc2023-lib/src/commonMain/kotlin/com/github/ephemient/aoc2023/Day6.kt
+++ kt/aoc2023-lib/src/commonMain/kotlin/com/github/ephemient/aoc2023/Day6.kt
@@ -1,12 +1,17 @@
 package com.github.ephemient.aoc2023
 
+import kotlin.math.ceil
+import kotlin.math.floor
+import kotlin.math.roundToInt
+import kotlin.math.sqrt
+
 class Day6(input: String) {
     private val races = input.lines().let { (line1, line2) ->
         NUMBER.findAll(line1).map { it.value.toInt() } zip
             NUMBER.findAll(line2).map { it.value.toInt() }
     }
 
-    fun part1(): Int = 0
+    fun part1(): Int = races.sumOf { (time, distance) -> winCount(time, distance) }
 
     fun part2(): Int = 0
 
@@ -14,7 +19,13 @@ class Day6(input: String) {
         private val NUMBER = """\d+""".toRegex()
 
         private fun winCount(time: Int, distance: Int): Int {
-            // wait
+            // wait * (time - wait) > distance
+            // wait * wait - time * wait < -distance
+            // (wait - time/2)^2 < time^2/4 + distance
+            // wait = time/2 ± sqrt(time^2/4 + distance)
+            val lo = ceil(time / 2.0 - sqrt(time * time / 4.0 + distance)).roundToInt()
+            val hi = floor(time / 2.0 + sqrt(time * time / 4.0 + distance)).roundToInt()
+            return hi - lo + 1
         }
     }
 }
