6f39bb4dacb82b2acffbfdc2f089c0a607abf13b
Daniel Lin <ephemient@gmail.com>
2023-12-12 16:06:05 -0500

    Revert back to faster implementation

--- hs/src/Day12.hs
+++ hs/src/Day12.hs
@@ -2,35 +2,64 @@
 Module:         Day12
 Description:    <https://adventofcode.com/2023/day/12 Day 12: Hot Springs>
 -}
-{-# LANGUAGE MultiWayIf, OverloadedStrings, ViewPatterns #-}
+{-# LANGUAGE BlockArguments, LambdaCase, MultiWayIf, OverloadedStrings, TransformListComp, ViewPatterns #-}
 module Day12 (part1, part2) where
 
 import Common (readEntire)
+import Control.Monad (forM_, when)
+import Control.Monad.State (MonadState, evalState, execStateT, gets, modify, modify')
+import Control.Monad.Trans (lift)
 import Control.Parallel.Strategies (parMap, rseq)
-import Data.List (foldl')
+import Data.List (foldl', maximumBy, inits, tails, scanl')
+import Data.Map (Map)
+import qualified Data.Map as Map (empty, insert, lookup)
+import Data.Ord (comparing)
 import Data.Text (Text)
-import qualified Data.Text as T (any, drop, findIndex, index, intercalate, length, lines, split, take, words)
+import qualified Data.Text as T (all, any, breakOn, count, drop, dropAround, dropEnd, dropWhile, dropWhileEnd, head, index, intercalate, length, lines, null, split, tail, take, unlines, unpack, words)
 import qualified Data.Text.Read as T (decimal)
-import Data.Vector.Unboxed (Vector)
-import qualified Data.Vector.Unboxed as V (drop, generate, sum, take)
+
+choose :: Int -> Int -> Int
+n `choose` r = foldl' f 1 $ zip [1..r] [n, n - 1..] where
+    f k (a, b) | (q, 0) <- (k * b) `divMod` a = q
+infix 1 `choose`
 
 solutions :: Text -> [Int] -> Int
-solutions string (reverse -> run0:runs) =
-    V.sum . maybe id (V.take . succ) (T.findIndex (== '#') string) $ foldl' f v0 runs where
-    v0 = V.generate (T.length string) $ \i -> if
-      | i + run0 > T.length string -> 0
-      | i /= 0, string `T.index` (i - 1) == '#' -> 0
-      | T.any (== '.') . T.take run0 $ T.drop i string -> 0
-      | T.any (== '#') $ T.drop (i + run0) string -> 0
-      | otherwise -> 1
-    f v run = V.generate (T.length string) $ \i -> if
-      | i + run >= T.length string -> 0
-      | i /= 0, string `T.index` (i - 1) == '#' -> 0
-      | T.any (== '.') . T.take run $ T.drop i string -> 0
-      | string `T.index` (i + run) == '#' -> 0
-      | otherwise -> V.sum .
-            maybe id (V.take . succ) (T.findIndex (== '#') $ T.drop (i + run + 1) string) $
-            V.drop (i + run + 1) v
+solutions s xs = evalState (solutions' s xs) Map.empty where
+    solutions' (T.dropAround (== '.') -> s) xs = gets (Map.lookup (s, xs)) >>= flip maybe pure do
+        let m = sum xs
+            x:xs' = xs
+            maxRun = maximumBy (comparing T.length) $ T.split (/= '#') s
+        result <- if
+          | T.count "#" s > m || m > T.length s - T.count "." s || m + length xs - 1 > T.length s
+          -> pure 0
+          | T.null s || null xs -> pure 1
+          | (leftS, rightS) <- T.breakOn "." s, not $ T.null rightS -> flip execStateT 0 $
+            forM_
+              [ (leftXs, rightXs)
+              | (leftXs, rightXs, acc) <- zip3 (inits xs) (tails xs) $ scanl' ((+) . succ) (-1) xs
+              , then takeWhile by acc <= T.length leftS
+              ] $ \(leftXs, rightXs) -> lift (solutions' leftS leftXs) >>= \case
+                0 -> pure ()
+                left -> lift (solutions' rightS rightXs) >>= modify' . (+) . (left *)
+          | T.all (/= '#') s -> pure $ T.length s - m + 1 `choose` length xs
+          | T.length maxRun > maximum xs -> pure 0
+          | not $ T.null maxRun, (leftS, rightS) <- T.breakOn maxRun s -> flip execStateT 0 $
+            forM_
+              [ (T.dropEnd (dx + 1) leftS, leftXs, T.drop (x' - dx + 1) rightS, rightXs)
+              | (leftXs, x' : rightXs, acc) <- zip3 (inits xs) (tails xs) $ scanl' ((+) . succ) 0 xs
+              , dx <- [max 0 $ x' - T.length rightS..x' - T.length maxRun]
+              , then takeWhile by acc + dx <= T.length leftS
+              , dx + 1 > T.length leftS || leftS `T.index` (T.length leftS - dx - 1) /= '#'
+              , x' - dx >= T.length rightS || rightS `T.index` (x' - dx) /= '#'
+              ] $ \(leftS, leftXs, rightS, rightXs) -> lift (solutions' leftS leftXs) >>= \case
+                0 -> pure ()
+                left -> lift (solutions' rightS rightXs) >>= modify' . (+) . (left *)
+          | otherwise -> flip execStateT 0 $ do
+            when (x == T.length s || s `T.index` x /= '#') $
+                lift (solutions' (T.drop (x + 1) s) xs') >>= modify' . (+)
+            when (T.head s /= '#') $ lift (solutions' (T.tail s) xs) >>= modify' . (+)
+        modify $ Map.insert (s, xs) result
+        pure result
 
 part1 :: Text -> Int
 part1 = sum . parMap rseq part1' . T.lines where
